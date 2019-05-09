4fc1f7e2ab25ee62019ec2b6253b9d139121ed7d
yglin <yglin.mlanser@gmail.com>
2019-05-09 17:17:04 +0800

    change scheduler default route to schedule-form

--- apps/scheduler/src/app/routes.ts
+++ apps/scheduler/src/app/routes.ts
@@ -1,9 +1,10 @@
 import {Routes} from '@angular/router';
 import {SchedulerComponent} from './scheduler/scheduler.component';
+import { ScheduleFormComponent } from '@ygg/shared/domain/schedule';
 
 export const routes: Routes = [
   {
     path: 'scheduler',
-    children: [{path: '', pathMatch: 'full', component: SchedulerComponent}]
+    children: [{path: '', pathMatch: 'full', component: ScheduleFormComponent}]
   }
 ];
