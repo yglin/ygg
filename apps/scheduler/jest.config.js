4f263fa8d505ce62428e92fe1419b8ab4e7bea6e
Play What <playwhat.ygg@gmail.com>
2019-06-14 13:27:44 +0800

    Remove unnecessary  apps

--- apps/scheduler/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'scheduler',
-  preset: '../../jest.config.js',
-  coverageDirectory: '../../coverage/apps/scheduler/',
-  snapshotSerializers: [
-    'jest-preset-angular/AngularSnapshotSerializer.js',
-    'jest-preset-angular/HTMLCommentSerializer.js'
-  ]
-};
--- apps/user/frontend-e2e/src/support/app.po.ts
+++
@@ -1,8 +0,0 @@
-export const getGreeting = () => cy.get('h1');
-
-export const loginAnonymously = () => {
-  cy.get('button#login').click();
-  cy.wait(1000);
-  cy.get('div#login-dialog button#login-anonymous').click();
-};
-
