6976ed83a7f66d2ca0be409df1053aa52cf4dd86
yglin <yglin.mlanser@gmail.com>
2019-06-28 18:22:27 +0800

    Write e2e for admin/staff/agent page

--- apps/playwhat-e2e/src/integration/admin.spec.ts
+++ apps/playwhat-e2e/src/integration/admin.spec.ts
@@ -1,23 +1,28 @@
 import { loginAdmin, logout } from '../support/app.po';
 
-describe('Test admin link', () => {
+import { gotoAdminDashboard } from "../support/app.po";
+
+describe('Test admin links', () => {
+  
   beforeEach(function() {
-    cy.visit('/');
-    loginAdmin();
-    // Wait for login, this is not a promising solution here but, what the heck.
-    cy.wait(3000);
+    gotoAdminDashboard();
   });
 
   afterEach(function() {
     logout();
   });
 
-  it('should show admin link in user menu, and it should link to admin dashboard page', () => {
-    cy.get('#account-widget .menu-trigger').click();
-    cy.get('#user-menu button#admin').click();
+  it('should show admin dashboard as entry page', () => {
     cy.url().should('match', /.*\/admin$/);
     cy.get('#admin-dashboard').should('be.visible');
   });
+
+  it('should be able to follow path admin -> staff -> agent to agents setting', () => {
+    cy.get('#admin-dashboard #admin-staff').click();
+    cy.get('#admin-staff #admin-staff-agent').click();
+    cy.get('#admin-staff-agent #user-selector').should('be.visible');
+  });
+  
 });
 
 // describe('Admin securities', () => {
--- apps/playwhat-e2e/src/support/app.po.ts
+++ apps/playwhat-e2e/src/support/app.po.ts
@@ -8,3 +8,13 @@ export function logout() {
   // @ts-ignore
   cy.logout();
 }
+
+export function gotoAdminDashboard() {
+  cy.visit('/');
+  loginAdmin();
+  // Wait for initial auto login,
+  // this is definitely an ugly workaround right now, but what the heck.
+  cy.wait(3000);
+  cy.get('#account-widget .menu-trigger').click();
+  cy.get('#user-menu button#admin').click();
+}
