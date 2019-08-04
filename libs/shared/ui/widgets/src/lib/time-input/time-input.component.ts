ddf5736bd12300113ae5fc647ceb5b4594072425
yglin <yglin.mlanser@gmail.com>
2019-08-04 18:50:23 +0800

    Done time-input control

---
+++ libs/shared/ui/widgets/src/lib/time-input/time-input.component.ts
@@ -0,0 +1,105 @@
+import * as moment from 'moment';
+import { noop, clamp } from 'lodash';
+import { Component, OnInit, Input, forwardRef, OnDestroy } from '@angular/core';
+import {
+  ControlValueAccessor,
+  NG_VALUE_ACCESSOR,
+  FormControl
+} from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { AmazingTimePickerService } from 'amazing-time-picker';
+
+@Component({
+  selector: 'ygg-time-input',
+  templateUrl: './time-input.component.html',
+  styleUrls: ['./time-input.component.css'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => TimeInputComponent),
+      multi: true
+    }
+  ]
+})
+export class TimeInputComponent implements ControlValueAccessor, OnDestroy {
+  @Input() label = '';
+  inputHourControl: FormControl = new FormControl(0);
+  inputMinuteControl: FormControl = new FormControl(0);
+  emitChange: (value: Date) => any = noop;
+  subscriptions: Subscription[] = [];
+
+  _time: Date = new Date();
+  set time(value: Date) {
+    if (value) {
+      this._time = value;
+      this.emitChange(this._time);
+    }
+  }
+  get time(): Date {
+    return this._time;
+  }
+
+  constructor(
+    private amazingTimePickerService: AmazingTimePickerService
+  ) {
+    this.subscriptions.push(
+      this.inputHourControl.valueChanges.subscribe(hour => {
+        const justifiedValue = clamp(hour, 0, 23);
+        if (justifiedValue !== hour) {
+          this.inputHourControl.setValue(justifiedValue, { emitEvent: false });
+        }
+        this.setHour(justifiedValue);
+      })
+    );
+    this.subscriptions.push(
+      this.inputMinuteControl.valueChanges.subscribe(minute => {
+        const justifiedValue = clamp(minute, 0, 59);
+        if (justifiedValue !== minute) {
+          this.inputMinuteControl.setValue(justifiedValue, {
+            emitEvent: false
+          });
+        }
+        this.setMinute(justifiedValue);
+      })
+    );
+  }
+
+  ngOnDestroy() {
+    for (const subscription of this.subscriptions) {
+      subscription.unsubscribe();
+    }
+  }
+
+  writeValue(value: Date) {
+    if (value) {
+      this._time = value;
+    }
+  }
+
+  registerOnChange(fn) {
+    this.emitChange = fn;
+  }
+
+  registerOnTouched() {}
+
+  setHour(hour: number) {
+    this._time.setHours(hour);
+    this.emitChange(this._time);
+  }
+
+  setMinute(minute: number) {
+    this._time.setMinutes(minute);
+    this.emitChange(this._time);
+  }
+
+  openTimePicker() {
+    const amazingTimePicker = this.amazingTimePickerService.open({
+      time: moment(this.time).format('HH:mm')
+    });
+    amazingTimePicker.afterClose().subscribe(timeToken => {
+      if (timeToken) {
+        this.time = moment(timeToken, 'HH:mm').toDate();
+      }
+    });
+  }
+}
