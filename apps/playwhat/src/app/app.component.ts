9cf281ec3443034828f4de374bd1b150b607f2a0
yglin <yglin.mlanser@gmail.com>
2019-10-21 19:04:25 +0800

    go-to-top-button wait 3 sec and get the hell out of way

--- apps/playwhat/src/app/app.component.ts
+++ apps/playwhat/src/app/app.component.ts
@@ -1,5 +1,12 @@
-import { Component, OnInit, OnDestroy, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
-import { ViewportScroller } from "@angular/common";
+import {
+  Component,
+  OnInit,
+  OnDestroy,
+  AfterViewInit,
+  ViewChild,
+  ElementRef
+} from '@angular/core';
+import { ViewportScroller } from '@angular/common';
 import { Router, ActivatedRoute, NavigationEnd } from '@angular/router';
 import {
   AuthenticateService,
@@ -8,8 +15,8 @@ import {
   UserMenuItem,
   User
 } from '@ygg/shared/user';
-import { Subscription, of, fromEvent } from 'rxjs';
-import { switchMap, filter } from 'rxjs/operators';
+import { Subscription, of, fromEvent, BehaviorSubject } from 'rxjs';
+import { switchMap, filter, auditTime, tap, debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'pw-root',
@@ -21,8 +28,9 @@ export class AppComponent implements OnInit, OnDestroy, AfterViewInit {
   loggedIn = false;
   subscriptions: Subscription[] = [];
   pageClass = 'pw-page';
-  isOnTop = true;
-  @ViewChild('page', {static: false}) pageElement: ElementRef;
+  showGoToTopButton$: BehaviorSubject<boolean> = new BehaviorSubject(false);
+  showGoToTopButton = false;
+  @ViewChild('page', { static: false }) pageElement: ElementRef;
 
   constructor(
     private authenticateService: AuthenticateService,
@@ -67,25 +75,40 @@ export class AppComponent implements OnInit, OnDestroy, AfterViewInit {
     // Register user-menu-item admin
     this.subscriptions.push(subscription);
 
-    subscription = this.router.events.pipe(
-      filter(e => e instanceof NavigationEnd)
-    ).subscribe((event: NavigationEnd) => {
-      // console.log(event.url);
-      if (/^\/?admin(\/.+)*$/.test(event.url)) {
-        this.pageClass = 'pw-page-admin';
-      } else {
-        this.pageClass = 'pw-page';
-      }
-    });
+    subscription = this.router.events
+      .pipe(filter(e => e instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        // console.log(event.url);
+        if (/^\/?admin(\/.+)*$/.test(event.url)) {
+          this.pageClass = 'pw-page-admin';
+        } else {
+          this.pageClass = 'pw-page';
+        }
+      });
     this.subscriptions.push(subscription);
   }
 
   ngAfterViewInit() {
     if (this.pageElement) {
-      fromEvent(this.pageElement.nativeElement, 'scroll').subscribe(() => {
-        // console.log(this.pageElement.nativeElement.scrollTop);
-        this.isOnTop = this.pageElement.nativeElement.scrollTop <= 100;
-      });
+      this.subscriptions.push(
+      fromEvent(this.pageElement.nativeElement, 'scroll')
+        .pipe(auditTime(500))
+        .subscribe(() => {
+          const isOnTop = this.pageElement.nativeElement.scrollTop <= 100;
+          if (!isOnTop && !this.showGoToTopButton) {
+            this.showGoToTopButton$.next(true);
+          } else {
+            this.showGoToTopButton$.next(false);
+          }
+        }));
+      this.subscriptions.push(this.showGoToTopButton$.pipe(
+        tap(value => this.showGoToTopButton = value),
+        debounceTime(3000),
+      ).subscribe(() => {
+        if (this.showGoToTopButton) {
+          this.showGoToTopButton = false;
+        }
+      }));
     }
   }
 
