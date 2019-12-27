23cd09dd08839af59ddce445266a2b52c7cb9694
yglin <yglin.mlanser@gmail.com>
2019-12-27 16:21:31 +0800

    Revise the-thing create e2e spec

--- apps/the-thing-e2e/src/integration/create-the-thing.spec.ts
+++ apps/the-thing-e2e/src/integration/create-the-thing.spec.ts
@@ -1,44 +1,123 @@
-import { siteNavigator } from "../support/site-navigator";
-import { TheThingCell } from "@ygg/the-thing/core";
-import { TheThingCellListPageObjectCypress } from "../page-objects/cell";
+enum TheThingCellType {
+  text = 'text',
+  longtext = 'longtext'
+}
+
+interface TheThingCell {
+  name: string;
+  type: string;
+  value: any;
+}
+
+function setValueText(value: string) {
+  cy.get('.new-cell .value input')
+    .clear()
+    .type(value);
+}
+
+function setValueLongtext(value: string) {
+  cy.get('.new-cell .value textarea')
+    .clear()
+    .type(value);
+}
+
+function addCell(cell: TheThingCell) {
+  cy.get('.new-cell .name input')
+    .clear()
+    .type(cell.name);
+  cy.get('.new-cell .type-select').select(cell.type);
+  switch (cell.type) {
+    case 'text':
+      setValueText(cell.value);
+      break;
+    case 'longtext':
+      setValueLongtext(cell.value);
+      break;
+    default:
+      break;
+  }
+  cy.get('button.add-cell').click();
+}
+
+function expectCellText(cell: TheThingCell) {
+  cy.get(`.cell[name="${cell.name}"] .value`).contains(cell.value);
+}
+
+const expectCelllongtext = expectCellText;
+
+function expectCell(cell: TheThingCell) {
+  switch (cell.type) {
+    case TheThingCellType.text:
+      expectCellText(cell);
+      break;
+
+    case TheThingCellType.longtext:
+      expectCelllongtext(cell);
+      break;
+
+    default:
+      break;
+  }
+}
 
 describe('Create a new the-thing', () => {
+  const theThing: any = {
+    types: ['play', 'lesson', 'travel', 'food'],
+    name: 'The Thing 1982',
+    cells: [
+      {
+        name: 'title',
+        type: 'text',
+        value: 'John Carpenter\'s The Thing'
+      },
+      {
+        name: 'introduction',
+        type: 'longtext',
+        value:
+          'A US research station, Antarctica, early-winter 1982. The base is suddenly buzzed by a helicopter from the nearby Norwegian research station. They are trying to kill a dog that has escaped from their base. After the destruction of the Norwegian chopper the members of the US team fly to the Norwegian base, only to discover them all dead or missing. They do find the remains of a strange creature the Norwegians burned. The Americans take it to their base and deduce that it is an alien life form. After a while it is apparent that the alien can take over and assimilate into other life forms, including humans, and can spread like a virus. This means that anyone at the base could be inhabited by The Thing, and tensions escalate.'
+      }
+    ]
+  };
 
-  it('Create cell, with the derived form create a new the-thing', () => {
-    // Forge test data
-    const cellWhole = TheThingCell.forge({
-      DNAs: 'all'
+  it('Create a new the-thing, confirm data consistency', () => {
+    // Navigate to the-thing creation page
+    cy.visit('/the-thing/create');
+
+    // Choose type tags of the-thing
+    cy.wrap(theThing.types).each((type: string) => {
+      cy.get('.types input')
+        .clear()
+        .type(type);
+      cy.get('.types button.add').click();
     });
-    const theThing = cellWhole.forgeTheThing();
 
-    // Navigate to Cell creation page
-    siteNavigator.goto(['cells']);
-    const theThingCellListPO = new TheThingCellListPageObjectCypress();
-    theThingCellListPO.gotoCreatePage();
+    // Input name of the thing
+    cy.get('.name input')
+      .clear()
+      .type(theThing.name);
 
-    // // Create a Cell with all DNAs, as Cell-whole
-    // theThingCellEditPO = new TheThingCellEditPageObjectCypress();
-    // for (const DNA of DNAs) {
-    //   theThingCellEditPO.addDNA(DNA);
-    // }
-    // theThingCellEditPO.submit();
+    // Add cells
+    cy.wrap(theThing.cells).each((cell: any) =>
+      addCell(cell)
+    );
 
-    // // Navigate to form of Cell-whole
-    // siteNavigator.goto(['cells']);
-    // theThingCellListPO.clickOnCell(cellWhole);
-    // const theThingCellViewPO = new TheThingCellViewPageObjectCypress();
-    // theThingCellViewPO.gotoCreatePage();
+    // Submit
+    cy.get('button.submit').click();
 
-    // // Create a new the-thing with the form
-    // const theThingEditPO = new TheThingEditPageObjectCypress();
-    // theThingEditPO.setValue(theThing);
-    // theThingEditPO.submit();
+    // Wait for navigating to view page
+    cy.location({ timeout: 10000 }).should('match', /.*\/the-thing\/.+/);
 
-    // // Navigate to the view of new the-thing
-    // // Automatically redirect to view when submitted in edit page
-    // const theThingViewPO = new theThingViewPageObjectCypress();
+    // Expect types
+    cy.wrap(theThing.types).each((type: string) => {
+      cy.get('.types').contains(type);
+    });
 
-    // // Confirm data consistensy
-    // theThingViewPO.expectValue(theThing);
+    // Expect name
+    cy.get('.name').contains(theThing.name);
+
+    // Expect cells
+    cy.wrap(theThing.cells).each((cell: any) => {
+      expectCell(cell);
+    });
   });
 });
--- apps/the-thing-e2e/src/integration/generate-view.spec.ts
+++
@@ -1,7 +0,0 @@
-describe('Generate view from the-thing model and value', () => {
-  it('Should show label of the-thing', () => {
-  });
-
-  it('Should show correct value of corresponding type of each property', () => {
-  });  
-});
