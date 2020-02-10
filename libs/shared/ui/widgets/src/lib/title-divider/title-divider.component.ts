596a2052cc2e151a84ccc088f12e1f6a9d3c2a73
yglin <yglin.mlanser@gmail.com>
2020-02-10 14:01:17 +0800

    Leveled ygg-title-divider

--- libs/shared/ui/widgets/src/lib/title-divider/title-divider.component.ts
+++ libs/shared/ui/widgets/src/lib/title-divider/title-divider.component.ts
@@ -1,9 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+const MAX_LEVEL = 10;
+const MAX_FONT_SIZE = 24;
+const MIN_FONT_SIZE = 12;
 
 @Component({
   selector: 'ygg-title-divider',
   templateUrl: './title-divider.component.html',
   styleUrls: ['./title-divider.component.css']
 })
-export class TitleDividerComponent {
+export class TitleDividerComponent implements OnInit {
+  @Input() level: number = 1;
+  styles: any = {};
+  maxFontSize: number = MAX_FONT_SIZE;
+  minFontSize: number = MIN_FONT_SIZE;
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.level = Math.min(this.level, MAX_LEVEL);
+    const leveledFontSize = 
+      MIN_FONT_SIZE +
+      Math.floor(
+        ((MAX_FONT_SIZE - MIN_FONT_SIZE) * (MAX_LEVEL - this.level + 1)) /
+          MAX_LEVEL
+      );
+    this.minFontSize = Math.max(MIN_FONT_SIZE, leveledFontSize - 2);
+    this.maxFontSize = Math.min(MAX_FONT_SIZE, leveledFontSize + 2);
+    const width = (90 * (MAX_LEVEL - this.level + 1)) / MAX_LEVEL;
+    this.styles = {
+      width: width + '%',
+      'min-width': '300px'
+    };
+    console.dir(this.level);
+    console.dir(this.styles);
+  }
 }
