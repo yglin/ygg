dcf7506eab39c121465a42b74282a59716bbf695
yglin <yglin.mlanser@gmail.com>
2020-03-28 18:10:41 +0800

    Refine behavior of ygg-title-divider

--- libs/shared/ui/widgets/src/lib/title-divider/title-divider.component.ts
+++ libs/shared/ui/widgets/src/lib/title-divider/title-divider.component.ts
@@ -15,6 +15,7 @@ export class TitleDividerComponent implements OnInit {
   dividerStyle: any = {};
   maxFontSize: number = MAX_FONT_SIZE;
   minFontSize: number = MIN_FONT_SIZE;
+  titleSize: number;
 
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
@@ -28,12 +29,13 @@ export class TitleDividerComponent implements OnInit {
       );
     this.minFontSize = Math.max(MIN_FONT_SIZE, leveledFontSize - 2);
     this.maxFontSize = Math.min(MAX_FONT_SIZE, leveledFontSize + 2);
+    this.titleSize = Math.floor((this.minFontSize + this.maxFontSize) / 2);
     const width = Math.floor((90 * (MAX_LEVEL - this.level + 1)) / MAX_LEVEL);
     const borderWidth =
       1 + Math.floor((4 * (MAX_LEVEL - this.level + 1)) / MAX_LEVEL);
-    this.styles = {
-      width: width + '%',
-    };
+    // this.styles = {
+    //   width: width + '%',
+    // };
     this.dividerStyle = {
       'border-width': `${borderWidth}px`
     };
