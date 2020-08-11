c4e5b73f99f89e6fb31f9f7111794ec22ddd00e3
yglin <yglin.mlanser@gmail.com>
2020-08-11 11:53:18 +0800

    Remove profile in user-menu

--- libs/shared/user/ui/src/lib/components/user-menu/user-menu.service.ts
+++ libs/shared/user/ui/src/lib/components/user-menu/user-menu.service.ts
@@ -14,16 +14,16 @@ export class UserMenuService {
   constructor(private authenticateService: AuthenticateService) {
     this.menuItems = {};
     this.menuItems$ = new BehaviorSubject(values(this.menuItems));
-    this.authenticateService.currentUser$.subscribe(user => {
-      if (user && user.id) {
-        this.addItem({
-          id: 'profile',
-          icon: 'account_box',
-          label: '個人資料',
-          link: 'users/me'
-        });
-      }
-    });
+    // this.authenticateService.currentUser$.subscribe(user => {
+    //   if (user && user.id) {
+    //     this.addItem({
+    //       id: 'profile',
+    //       icon: 'account_box',
+    //       label: '個人資料',
+    //       link: 'users/me'
+    //     });
+    //   }
+    // });
   }
 
   addItem(menuItem: UserMenuItem) {
