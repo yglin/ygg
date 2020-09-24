0ff9c52ef69d8a5e4abd759ce9e0568c163b832f
yglin <yglin.mlanser@gmail.com>
2020-09-24 11:40:48 +0800

    build-functions.sh: add usage hint

--- tools/scripts/firebase/build-functions.sh
+++ tools/scripts/firebase/build-functions.sh
@@ -1,8 +1,19 @@
-#! /bin/bash -x
+#! /bin/bash
 
 project=$1
 target=$2
-rootDir=`pwd`
+rootDir=$(pwd)
+
+if [ -z "$project" ] || [ -z "$target" ]
+then
+  echo "=================================================="
+  echo "Usage: build-functions.sh [PROJECT] [TARGET]"
+  echo "PROJECT = ourbox | playwhat"
+  echo "TARGET = local | develop"
+  echo "=================================================="
+  echo ""
+  exit 1
+fi
 
 # Setup environment to develop
 firebase use default
@@ -10,3 +21,5 @@ node tools/scripts/setup-environments.js $project $target
 
 # build
 ng build functions --prod
+
+exit 0
