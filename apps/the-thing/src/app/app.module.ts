a17f57e4708b5c0b96dbb9701cbd23dc461791ae
yglin <yglin.mlanser@gmail.com>
2020-11-29 11:38:07 +0800

    Get the-thing app work again

--- apps/the-thing/src/app/app.module.ts
+++ apps/the-thing/src/app/app.module.ts
@@ -1,33 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, Injector, APP_INITIALIZER } from '@angular/core';
-
-import { SharedUiNgMaterialModule } from '@ygg/shared/ui/ng-material';
-import { SharedUiWidgetsModule } from '@ygg/shared/ui/widgets';
-import {
-  TheThingUiModule,
-  TheThingEditorComponent,
-  TheThingViewComponent,
-  MyThingsComponent,
-  TheThingFinderComponent
-} from '@ygg/the-thing/ui';
-
-import { RouterModule } from '@angular/router';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './layout/header/header.component';
-import { HomeComponent } from './pages/home/home.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
 // import { CellsComponent } from './pages/cell/cells/cells.component';
 // import { CellEditComponent } from './pages/cell/cell-edit/cell-edit.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
 import { SharedInfraDataAccessModule } from '@ygg/shared/infra/data-access';
-import { TheThingResolver } from '@ygg/the-thing/ui';
-import { LoggedInGuard } from "@ygg/shared/user/ui";
+import { SharedUiNgMaterialModule } from '@ygg/shared/ui/ng-material';
+import { SharedUiWidgetsModule } from '@ygg/shared/ui/widgets';
+import { TheThingUiModule } from '@ygg/the-thing/ui';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CreateComponent } from './pages/create/create.component';
+import { routes } from './route';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    CreateComponent
     // CellsComponent,
     // CellEditComponent,
     // TheThingEditorComponent,
@@ -41,8 +34,8 @@ import { LoggedInGuard } from "@ygg/shared/user/ui";
     SharedUiNgMaterialModule,
     SharedUiWidgetsModule,
     TheThingUiModule,
-    RouterModule.forRoot([]),
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ apps/the-thing/src/app/route.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { CreateComponent } from './pages/create/create.component';
+
+export const routes: Routes = [
+  {
+    path: 'create',
+    component: CreateComponent
+  }
+];
